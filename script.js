fb1a27232a9716ff4c38924b31892dd5686f169e
swathireddy369 <swathiamaravadi53@gmail.com>
2024-10-16 20:40:40 -0400

    implemented stylings for snake_game

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("hello snake");
+//consts
+let highScore,currentScore = 0;
+let snake=[{x:10,y:10},{x:10,y:11},{x:10,y:12}];
+let food = [{x:14,y:14}]
+let hasGameStarted = flase
+
+//html elements
+
+//methods
+const drawSnake=()=>{
+
+}
+const generateFood = ()=>{
+
+}
+const move= ()=>{
+    
+}
